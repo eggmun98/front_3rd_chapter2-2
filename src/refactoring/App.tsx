e457724e1d8b3f8bf4494bdb53d06120bfc378a7
eggmun <eggmun98@gmail.com>
2024-10-25 05:13:30 +0900

    refactor: 불필요한 import 제거

--- src/refactoring/App.tsx
+++ src/refactoring/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { CartPage } from './components/CartPage.tsx';
 import { AdminPage } from './components/AdminPage.tsx';
 import { Coupon, Product } from '../types.ts';

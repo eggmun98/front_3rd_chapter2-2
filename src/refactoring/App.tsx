8d76fd72d2ff82c8a30fed0a504e8fb3ddc201c9
eggmun <eggmun98@gmail.com>
2024-10-25 05:22:48 +0900

    refactor: Main 컴포넌트 사용

--- src/refactoring/App.tsx
+++ src/refactoring/App.tsx
@@ -1,34 +1,14 @@
-
-import { Coupon, Product } from '../types.ts';
-import { useCoupons, useProducts } from "./hooks";
-import { initialProducts } from './contents/initialProducts.ts';
-import { initialCoupons } from './contents/initialCoupons.ts';
 import { useToggle } from './hooks/useToggle.ts';
 import Navigation from './components/Layouts/Navigation/Navigation.tsx';
-import { AdminPage } from './components/Pages/Admin/AdminPage.tsx';
-import { CartPage } from './components/Pages/Card/CartPage.tsx';
+import Main from './components/Layouts/Main/Main.tsx';
 
 const App = () => {
-  const { products, updateProduct, addProduct } = useProducts(initialProducts);
-  const { coupons, addCoupon } = useCoupons(initialCoupons);
   const {value: isAdmin, toggle: onClickToggleAdmin} = useToggle()
 
   return (
     <div className="min-h-screen bg-gray-100">
       <Navigation isAdmin={isAdmin} onClickToggleAdmin={onClickToggleAdmin}/>
-      <main className="container mx-auto mt-6">
-        {isAdmin ? (
-          <AdminPage
-            products={products}
-            coupons={coupons}
-            onProductUpdate={updateProduct}
-            onProductAdd={addProduct}
-            onCouponAdd={addCoupon}
-          />
-        ) : (
-          <CartPage products={products} coupons={coupons}/>
-        )}
-      </main>
+      <Main isAdmin={isAdmin}/>
     </div>
   );
 };
